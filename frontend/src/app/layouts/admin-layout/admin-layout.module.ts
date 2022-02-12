fc7cb4e821d1fc28d8d40e171fb94a14ef80e34e
Oskar Sęk <oskar.sek@itpendent.com>
2022-02-12 03:22:22 +0100

    Wyrzucam najbardziej zbędne plikie

--- frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AdminLayoutRoutes} from './admin-layout.routing';
+import {DashboardComponent} from '../../dashboard/dashboard.component';
+import {NotificationsComponent} from '../../notifications/notifications.component';
+import {UpgradeComponent} from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -33,16 +28,12 @@ import {NgxFilesizeModule} from 'ngx-filesize';
         MatTooltipModule,
         NgxFilesizeModule,
     ],
-  declarations: [
-    DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
-  ]
+    declarations: [
+        DashboardComponent,
+        NotificationsComponent,
+        UpgradeComponent,
+    ]
 })
 
-export class AdminLayoutModule {}
+export class AdminLayoutModule {
+}
